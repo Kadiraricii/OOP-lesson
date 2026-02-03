e94185c7a7490fb91e55b47e8f5b76c88ffd37c4
Kadir Arıcı <mehmetkadira0@gmail.com>
2026-02-04 00:04:15 +0300

    Lesson 1: basic variables exercise

---
+++ lesson1/src/firstlesson.java
@@ -0,0 +1,31 @@
+public class firstlesson { //class= sınıf
+
+
+
+    //psvm
+    public static void main(String[] args) {
+        System.out.println(101);
+        System.out.println("deneme");
+
+        /* FORMUL
+        tip degiskenAdi = degiskenin verisi */
+
+        int yas=20;                  // tam sayilar icin = int - long
+        double kilo=70.1;           // kesirli sayilar icin = double - float
+
+        String ad="kadir";         // metin icin = String - char
+        String soyad="arici";      // metin icin = String - char
+
+        boolean resitmi=true;      //iki olasilik icin= boolean
+
+        // cogul veriler icin = diziler (arrays) ve listeler
+
+        System.out.println(yas);        //ekrana yası basar
+        System.out.println(kilo);       //ekrana kiloyu basar
+        System.out.println(ad);         //ekrana adi basar
+        System.out.println(soyad);      //ekrana soyadi basar
+        System.out.println(resitmi);    //ekrana true basar
+
+    }
+}
+
